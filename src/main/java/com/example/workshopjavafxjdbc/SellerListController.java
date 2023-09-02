cec52895193795af64a0c894c5dbb85eab60c522
Luiz Felipe <lfpereira323@gmail.com>
2023-09-01 23:37:53 -0300

    Department ComboBox

--- src/main/java/com/example/workshopjavafxjdbc/SellerListController.java
+++ src/main/java/com/example/workshopjavafxjdbc/SellerListController.java
@@ -101,7 +101,8 @@ public class SellerListController implements Initializable, DataChangeListener {
 
             SellerFormController controller = loader.getController();
             controller.setSeller(obj);
-            controller.setSellerService(new SellerService());
+            controller.setServices(new SellerService(), new DepartmentService());
+            controller.loadAssociatedObjects();
             controller.subscribeDataChangeListener(this);
             controller.updateFormData();
 
@@ -114,6 +115,7 @@ public class SellerListController implements Initializable, DataChangeListener {
             dialogStage.showAndWait();
 
         } catch (IOException e) {
+            e.printStackTrace();
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
     }
